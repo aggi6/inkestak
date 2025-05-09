284c0cae0d60b14f01f0ef88d3bdef561c177c24
oihan aginaga <oihanaginaga9@gmail.com>
2025-05-09 11:39:15 +0200

    Document bistaratzailea konponentera pasa WIP

---
+++ resources/js/components/Document.vue
@@ -0,0 +1,31 @@
+<template>
+    <tr>
+        <td>{{ document.id }}</td>
+        <td>{{ document.name }}</td>
+        <td>
+        <span>
+            {{ document.active ? 'Active' : 'Inactive' }}
+        </span>
+        </td>
+        <td>
+        <button v-if="document.can_download">
+            <i class="fas fa-download"></i> 
+        </button>
+        <span v-else>
+            <i class="fas fa-ban"></i> 
+        </span>
+        </td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        name: 'Document',
+        props: {
+            document: {
+                type: Object,
+                required: true,
+            },
+        },
+    };
+</script>
