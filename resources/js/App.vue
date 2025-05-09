35401f6506ff8aaa7a160146939825bf668dfa05
oihan aginaga <oihanaginaga9@gmail.com>
2025-05-09 10:46:20 +0200

    14191 Formularioa

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -33,21 +33,24 @@
         </tr>
       </tbody>
     </table>
-    <br>
     <button @click="erakutsi = !erakutsi">Gehitu dokumentua</button>
+    <br>
+    <br>
+
     <div v-show="erakutsi">
-      <form>
+      <form @submit.prevent="dokumentuaGehitu">
         <label>Izena</label>
-        <input type="text"/>
+        <input type="text" v-model="formData.name" />
         <br>
         <label>Path</label>
-        <input type="text"/>
+        <input type="text" v-model="formData.path" />
         <br>
         <label>Deskargatu</label>
-        <input type="checkbox"/>
+        <input type="checkbox" v-model="formData.can_download" />
         <br>
         <label>Aktibatu</label>
-        <input type="checkbox"/>
+        <input type="checkbox" v-model="formData.active" />
+        <button :disabled="!ahalGehitu">Bidali</button>
       </form>
     </div>
   </div>
@@ -59,6 +62,12 @@ export default {
   data() {
     return {
       erakutsi: false,
+      formData: {
+        name: '',
+        path: '',
+        can_download: false,
+        active: false,
+      },
       documents: [
         {
           id: 1,
@@ -76,7 +85,32 @@ export default {
         }
       ]
     }
-  }
+  },
+  computed: {
+    ahalGehitu() {
+      return (
+        this.formData.name.trim() !== '' && this.formData.path.trim() !== ''
+      );
+    }
+  },
+  methods: {
+    dokumentuaGehitu() {
+      const dokumentuBerria = {
+        id: this.documents.length + 1,
+        ...this.formData,
+      };
+      this.documents.push(dokumentuBerria);
+      this.garbituForm();
+    },
+    garbituForm() {
+      this.formData = {
+        name: '',
+        path: '',
+        can_download: false,
+        active: false,
+      };
+    },
+  },
 }
 </script>
 
